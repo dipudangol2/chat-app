445104d3cd050c844d4d72a9122b54d5af141e57
dipudangol2 <dipudangol2@gmail.com>
2025-06-29 22:14:30 +0545

    feat: Implement Contact list

--- server/controllers/ContactsController.js
+++ server/controllers/ContactsController.js
@@ -1,4 +1,6 @@
+import mongoose from "mongoose";
 import User from "../models/UserModel.js";
+import Message from "../models/MessagesModel.js";
 
 export const searchContacts = async (request, response, next) => {
     try {
@@ -9,7 +11,8 @@ export const searchContacts = async (request, response, next) => {
         }
 
         const sanitizedSearchTerm = searchTerm.replace(
-            /[.*+?^${}()|[\]\\]/g, "\\$&"
+            /[.*+?^${}()|[\]\\]/g,
+            "\\$&"
         );
 
         const regex = new RegExp(sanitizedSearchTerm, "i");
@@ -18,7 +21,7 @@ export const searchContacts = async (request, response, next) => {
             $and: [
                 { _id: { $ne: request.userId } },
                 {
-                    $or: [{ firstName: regex }, { lastName: regex }, { email: regex }]
+                    $or: [{ firstName: regex }, { lastName: regex }, { email: regex }],
                 },
             ],
         });
@@ -29,3 +32,62 @@ export const searchContacts = async (request, response, next) => {
     }
 };
 
+export const getContactsForDMList = async (request, response, next) => {
+    try {
+        let { userId } = request;
+
+        userId = new mongoose.Types.ObjectId(userId);
+
+        const contacts = await Message.aggregate([
+            {
+                $match: {
+                    $or: [{ sender: userId }, { recipient: userId }],
+                },
+            },
+            {
+                $sort: { timestamp: -1 },
+            },
+            {
+                $group: {
+                    _id: {
+                        $cond: {
+                            if: { $eq: ["$sender", userId] },
+                            then: "$recipient",
+                            else: "$sender",
+                        },
+                    },
+                    lastMessageTime: { $first: "$timestamp" },
+                },
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "_id",
+                    foreignField: "_id",
+                    as: "contactInfo",
+                },
+            },
+            {
+                $unwind: "$contactInfo",
+            },
+            {
+                $project: {
+                    _id: 1,
+                    email: "$contactInfo.email",
+                    firstName: "$contactInfo.firstName",
+                    lastName: "$contactInfo.lastName",
+                    image: "$contactInfo.image",
+                    color: "$contactInfo.color",
+                },
+            },
+            {
+                $sort: { lastMessageTime: -1 },
+            },
+        ]);
+
+        return response.status(200).json({ contacts });
+    } catch (error) {
+        console.log({ error });
+        return response.status(500).send("Internal Server Error!");
+    }
+};
