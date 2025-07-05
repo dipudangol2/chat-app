1b77615d49ec2667a923614b05224ccab6795544
dipudangol2 <dipudangol2@gmail.com>
2025-07-05 23:16:18 +0545

    feat: Implement group chat

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -6,47 +6,60 @@ import { useAppStore } from "../store";
 const SocketContext = createContext(null);
 
 export const useSocket = () => {
-    return useContext(SocketContext);
+  return useContext(SocketContext);
 };
 
 export const SocketProvider = ({ children }) => {
-    const socket = useRef();
-    const { userInfo } = useAppStore();
+  const socket = useRef();
+  const { userInfo } = useAppStore();
 
-    useEffect(() => {
-        if (userInfo) {
-            socket.current = io(HOST, {
-                withCredentials: true,
-                query: { userId: userInfo.id },
-            });
-            socket.current.on("connect", () => {
-                console.log("Connected to socket.");
-            });
+  useEffect(() => {
+    if (userInfo) {
+      socket.current = io(HOST, {
+        withCredentials: true,
+        query: { userId: userInfo.id },
+      });
+      socket.current.on("connect", () => {
+        console.log("Connected to socket.");
+      });
 
-            const handleRecieveMessage = (message) => {
-                const { selectedChatData, selectedChatType, addMessage } = useAppStore.getState();
+      const handleRecieveMessage = (message) => {
+        const { selectedChatData, selectedChatType, addMessage } =
+          useAppStore.getState();
 
-                if (
-                    selectedChatType !== undefined &&
-                    (selectedChatData._id === message.sender._id ||
-                        selectedChatData._id === message.recipient._id)
-                ) {
-                    console.log("message recieved", message)
-                    addMessage(message);
-                }
-            };
-
-            socket.current.on("recieveMessage", handleRecieveMessage);
-
-            return () => {
-                socket.current.disconnect();
-            };
+        if (
+          selectedChatType !== undefined &&
+          (selectedChatData._id === message.sender._id ||
+            selectedChatData._id === message.recipient._id)
+        ) {
+          console.log("message recieved", message);
+          addMessage(message);
         }
-    }, [userInfo]);
+      };
+      const handlerecieveChannelMessage = (message) => {
+        const { selectedChatData, selectedChatType, addMessage } =
+          useAppStore.getState();
 
-    return (
-        <SocketContext.Provider value={socket.current}>
-            {children}
-        </SocketContext.Provider>
-    );
+        if (
+          selectedChatType !== undefined &&
+          selectedChatData._id === message.channelId
+        ) {
+          addMessage(message);
+        }
+      };
+
+      socket.current.on("recieveMessage", handleRecieveMessage);
+      socket.current.on("recieve-channel-message", handlerecieveChannelMessage);
+
+      return () => {
+        socket.current.disconnect();
+      };
+    }
+  }, [userInfo]);
+
+  return (
+    <SocketContext.Provider value={socket.current}>
+      {children}
+    </SocketContext.Provider>
+  );
 };
--- client/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -3,9 +3,9 @@ import { useAppStore } from "@/store/index";
 import { GET_ALL_MESSAGES_ROUTE, HOST } from "@/utils/constants";
 import moment from "moment";
 import React, { useEffect, useRef, useState } from "react";
-import { MdFolderZip } from "react-icons/md"
-import { IoMdArrowRoundDown } from "react-icons/io"
-import { IoCloseSharp } from "react-icons/io5"
+import { MdFolderZip } from "react-icons/md";
+import { IoMdArrowRoundDown } from "react-icons/io";
+import { IoCloseSharp } from "react-icons/io5";
 
 const MessageContainer = () => {
   //* variables
@@ -17,7 +17,7 @@ const MessageContainer = () => {
     selectedChatMessages,
     setSelectedChatMessages,
     setFileDownloadProgress,
-    setIsDownloading
+    setIsDownloading,
   } = useAppStore();
   const [showImage, setShowImage] = useState(false);
   const [imageURL, setImageURL] = useState(null);
@@ -52,12 +52,11 @@ const MessageContainer = () => {
     }
   }, [selectedChatMessages]);
 
-
   const checkIfImage = (filePath) => {
     const imageRegex =
       /\.(jpg|png|jpeg|gif|bmp|tiff|tif|webp|svg|ico|heic|heif)$/i;
     return imageRegex.test(filePath);
-  }
+  };
 
   const renderMessages = () => {
     let lastDate = null;
@@ -73,6 +72,7 @@ const MessageContainer = () => {
             </div>
           )}
           {selectedChatType === "contact" && renderDMMessages(message)}
+          {selectedChatType === "channel" && renderChannelMessages(message)}
         </div>
       );
     });
@@ -87,7 +87,7 @@ const MessageContainer = () => {
         const { loaded, total } = progressEvent;
         const percentCompleted = Math.round((loaded * 100) / total);
         setFileDownloadProgress(percentCompleted);
-      }
+      },
     });
     const urlBlob = window.URL.createObjectURL(new Blob([response.data]));
     const link = document.createElement("a");
@@ -99,77 +99,102 @@ const MessageContainer = () => {
     window.URL.revokeObjectURL(urlBlob);
     setIsDownloading(false);
     setFileDownloadProgress(0);
-  }
+  };
 
   const renderDMMessages = (message) => (
     <div
-      className={`${message.sender === selectedChatData._id ? "text-left" : "text-right"
-        } `}
+      className={`${
+        message.sender === selectedChatData._id ? "text-left" : "text-right"
+      } `}
     >
       {message.messageType === "text" && (
         <div
           className={`
-      ${message.sender !== selectedChatData._id
-              ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#8417ff]/50 "
-              : "bg-[#2a2b33]/5 text-white/80 border-[#fff]/20 "
-            } 
+      ${
+        message.sender !== selectedChatData._id
+          ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#8417ff]/50 "
+          : "bg-[#2a2b33]/5 text-white/80 border-[#fff]/20 "
+      } 
           border inline-block p-4 rounded my-1 max-w-[50%]  break-words whitespace-pre`}
         >
           {message.content}
         </div>
       )}
 
-      {
-        message.messageType === "file" && <div
+      {message.messageType === "file" && (
+        <div
           className={`
-      ${message.sender !== selectedChatData._id
-              ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#8417ff]/50 "
-              : "bg-[#2a2b33]/5 text-white/80 border-[#fff]/20 "
-            } 
+      ${
+        message.sender !== selectedChatData._id
+          ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#8417ff]/50 "
+          : "bg-[#2a2b33]/5 text-white/80 border-[#fff]/20 "
+      } 
           border inline-block p-2 rounded my-1 max-w-[50%] break-words whitespace-pre`}
         >
-          {
-            checkIfImage(message.fileUrl)
-              ? (<div className="cursor-pointer "
-                onClick={() => {
-                  setShowImage(true);
-                  setImageURL(message.fileUrl);
-                }}
-              >
-                <img
-                  src={`${HOST}/${message.fileUrl}`}
-                  height={300}
-                  width={300}
-
-                />
-              </div>)
-              : (
-                <div
-                  className="cursor-pointer flex items-center justify-center gap-2 "
-                  onClick={() => downloadFile(message.fileUrl)}>
-
-                  <span className="text-white/80 md:text-xl bg-black/20 rounded-full p-3">
-                    <MdFolderZip />
-                  </span>
-                  <span className="text-sm md:text-md text-start break-all text-wrap ">{message.fileUrl.split("/").pop()}</span>
-                  {/* <span className="bg-black/20 rounded-full p-3 md:text-xl hover:bg-black/50 cursor-pointer transition-all duration-300 hover:text-white"
+          {checkIfImage(message.fileUrl) ? (
+            <div
+              className="cursor-pointer "
+              onClick={() => {
+                setShowImage(true);
+                setImageURL(message.fileUrl);
+              }}
+            >
+              <img
+                src={`${HOST}/${message.fileUrl}`}
+                height={300}
+                width={300}
+              />
+            </div>
+          ) : (
+            <div
+              className="cursor-pointer flex items-center justify-center gap-2 "
+              onClick={() => downloadFile(message.fileUrl)}
+            >
+              <span className="text-white/80 md:text-xl bg-black/20 rounded-full p-3">
+                <MdFolderZip />
+              </span>
+              <span className="text-sm md:text-md text-start break-all text-wrap ">
+                {message.fileUrl.split("/").pop()}
+              </span>
+              {/* <span className="bg-black/20 rounded-full p-3 md:text-xl hover:bg-black/50 cursor-pointer transition-all duration-300 hover:text-white"
                     onClick={() => downloadFile(message.fileUrl)}
                   >
                     <IoMdArrowRoundDown />
                   </span> */}
-                </div>
-              )
-          }
+            </div>
+          )}
         </div>
-
-      }
+      )}
       <div className="text-xs text-gray-600">
         {moment(message.timestamp).format("LT")}
       </div>
     </div>
   );
 
-
+  const renderChannelMessages = (message) => {
+    console.log(message.sender._id, userInfo.id);
+    console.log(message.sender._id === userInfo.id);
+    return (
+      <div
+        className={`mt-5 ${
+          message.sender._id !== userInfo.id ? "text-left" : "text-right"
+        }`}
+      >
+        {message.messageType === "text" && (
+          <div
+            className={`${
+              message.sender._id === userInfo.id
+                ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#8417ff]/50 "
+                : "bg-[#2a2b33]/5 text-white/80 border-[#fff]/20 "
+            } 
+          border inline-block p-4 rounded my-1 max-w-[50%]  break-words whitespace-pre`}
+          >
+            {message.content}
+          </div>
+        )}
+      </div>
+    );
+  };
 
   return (
     <div className="flex-1 overflow-y-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full">
